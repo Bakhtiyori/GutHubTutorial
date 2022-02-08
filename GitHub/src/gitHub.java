8839787acb024696b91a984eaca97cb952116901
bakhtiyoriloikzoda <bakhtiyoriloikzoda@bakhtiyoris-mbp>
2022-02-07 21:23:07 -0500

    Pushing this project to GitHub

---
+++ GitHub/src/gitHub.java
@@ -0,0 +1,11 @@
+
+public class gitHub {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Add me to github");
+		
+
+	}
+
+}
---
+++ GitHub/src/gitHubTest.java
@@ -0,0 +1,14 @@
+
+public class gitHubTest {
+
+	public static void main(String[] args) {
+		
+		int numone = 22;
+		int numtwo = 23;
+		int finalNum = numone + numtwo;
+		System.out.println(" The final number = to " + finalNum);
+		
+
+	}
+
+}
